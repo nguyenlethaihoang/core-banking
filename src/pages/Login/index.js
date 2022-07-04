fa7b3c748cd7e330f16295a9a16f454b671e68b2
18520058 <18520058@ms.uit.edu.vn>
2022-07-05 06:44:20 +0700

    update code

---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+function Login() {
+    return (  
+        <h2>Login page</h2>
+    );
+}
+
+export default Login;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import Home from '~/pages/Home';
+import Login from '~/pages/Login';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/login', component: Login},
+]
+
+
+const privateRoutes = [
+    
+]
+
+export { publicRoutes, privateRoutes};
