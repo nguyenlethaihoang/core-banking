9f067bca64916ebbda66e9592c1839a36c1b9170
18520058 <18520058@ms.uit.edu.vn>
2022-07-05 06:50:31 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { publicRoutes } from '~/routes'
+// import { publicRoutes } from '~/routes'
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
+    // <Router>
+    //   <div className="App">
+    //       <Routes>
+    //           {publicRoutes.map((route, index) => {
+    //             const Page = route.component
+    //             return <Route key={index} path={route.path} element={<Page />} />;
+    //           })}
+    //       </Routes>
+    //   </div>
+    // </Router>
+
     <Router>
-      <div className="App">
-          <Routes>
-              {publicRoutes.map((route, index) => {
-                const Page = route.component
-                return <Route key={index} path={route.path} element={<Page />} />;
-              })}
-          </Routes>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/Login" element={<Login />}/>
+        </Routes>
       </div>
     </Router>
   );
